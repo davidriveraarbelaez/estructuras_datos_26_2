cb65220f1d1d08a2622df74b37255a0cb9003955
David Ricardo Rivera Arbeláez <davidriveraarbelaez@gmail.com>
2026-08-06 10:56:05 -0500

    Unidad 1

---
+++ Unidad_1/acceso_elemento.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int edades[5] = {20, 25, 30, 35, 40};
+    cout<<edades[0]<<endl; // Visualiza el primer elemento del arreglo o sea la primera edad
+}
